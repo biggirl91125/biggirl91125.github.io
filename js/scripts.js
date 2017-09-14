f34546d226f82fd8869220d36621f1d60be99d81
wangchao <chaowang211311@sohu-inc.com>
2017-09-14 19:00:55 +0800

    de3

--- js/scripts.js
+++ js/scripts.js
@@ -189,11 +189,13 @@ $(document).ready(function($) {
 	});
 });
 (function(){
-	const works={template:'<iframe src="./work.html"></iframe>'};
-	const vue={template:'<iframe src="./那些年我们用过的框架/初学Vue-js.html"></iframe>'};
-	const webview={template:'<iframe src="./移动端开发那些坑儿/前端js与webview交互.html"></iframe>'};
+	const index={template:'<iframe src="./2017/Work-diary/zoo.html"></iframe>'};
+	const works={template:'<iframe src="./2017/Work-diary/work.html"></iframe>'};
+	const vue={template:'<iframe src="./2017/Work-diary/那些年我们用过的框架/初学Vue-js.html"></iframe>'};
+	const webview={template:'<iframe src="./2017/Work-diary/移动端开发那些坑儿/前端js与webview交互.html"></iframe>'};
 
 	const routes=[
+		{path:'/',component:index},
 		{path:'/works',component:works},
 		{path:'/vue',component:vue},
 		{path:'/webview',component:webview}
