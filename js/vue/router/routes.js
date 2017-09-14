1ce77b8a1ec3687c1a67646b98c45fd21a36db38
wangchao <chaowang211311@sohu-inc.com>
2017-09-14 14:51:02 +0800

    vue route ci

---
+++ js/vue/router/routes.js
@@ -0,0 +1,41 @@
+/**
+ * Created by chaowang211311 on 2017/9/14.
+ */
+(function(){
+
+    // 注册组件
+    const Home={template:'<div>Welcome home!</div>'};
+    const About={template:'<div>About page!</div>'};
+    const routes=[
+        {path:'/',component:Home},
+        {path:'/about',component:About}
+    ];
+    // 创建根实例
+    const app=new Vue({
+        el: '#vue-demo',
+        routes:routes,
+        data:{
+            currentRoute:window.location.pathname,
+            activeClass:'active'
+        },
+        methods:{
+            init:function(){
+                window.addEventListener('popstate',function(){
+                    this.currentRoute=window.location.pathname;
+                });
+            },
+            isActive:function(){
+
+            },
+            go:function(ev){
+                console.log("h5 go");
+                ev.preventDefault();
+                this.$root.currentRoute=this.href;
+                window.history && window.history.pushState
+                && window.history.pushState(null,null,this.href);
+
+            }
+        }
+    });
+    app.init();
+})();
