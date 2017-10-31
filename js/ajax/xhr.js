92587201ac3b95aac06a5365eb6de4314583ac2f
wangchao <chaowang211311@sohu-inc.com>
2017-10-31 17:37:05 +0800

    style mod

--- js/ajax/xhr.js
+++ js/ajax/xhr.js
@@ -39,6 +39,9 @@ function ajax(){
         }
 
     };
+    //超时设置
+    xhr.timeout=1000;
+    xhr.ontimeout=function(){};
     xhr.open('get','example.txt',true);
     //可以在这里设置头部信息
     //xhr相关的头部字段：
@@ -51,5 +54,106 @@ function ajax(){
     //      host
     //      referer
     xhr.setRequestHeader('myHeader','123');
+    xhr.overrideMimeType('text/xml');
+
     xhr.send(null);
 }
+//url后添加查询字段
+function addURLParam(url,name,value){
+    url+= (url.indexOf('?') == -1?'?':'&');
+    url+=encodeURIComponent(name)+'='+encodeURIComponent(value);
+    return url;
+}
+//进度条事件，IE8+只支持load事件
+function progress(){
+    var xhr=createXHR();
+    xhr.onload=function(event){
+        if((xhr.status >= 200&&xhr.status<300)||xhr.status==304){
+            console.log(xhr.responseText);
+        }else{
+            console.log('Request was successful:'+xhr.status);
+        }
+    };
+
+    xhr.onprogress=function(event){
+        var divStatus=document.getElementById('status');
+        if(event.lengthComputable){
+            divStatus.innerHTML='Received '+event.position+' of '+event.totalSize +'bytes.'
+        }
+    };
+
+    xhr.open('get','dd.php',true);
+    xhr.send(null);
+}
+
+
+//IE对CORS的实现——XDR
+function xdr_ie_cors(){
+    var xdr=new XDomainRequest();
+    xdr.onload=function(){
+        console.log(xdr.responseText);
+    };
+    xdr.error=function(){
+        console.log('An error occurred.');
+    };
+    xdr.timeout=1000;
+    xdr.ontimeout=function(){
+        console.log('Request took too long.');
+    };
+
+    xdr.open('get','http://somewhere.com');
+
+    //post请求时，配合contentType属性使用
+    //xdr.contentType='application/x-www-form-urlencoded';
+
+    xdr.send(null);
+}
+//其他浏览器对CORS的实现——XHR+绝对url
+function xhr_others_cors(){
+    ajax_cors();
+}
+
+//不能使用setRequestHeader设置自定义头部
+//getAllResponseHeaders()返回空字符串
+function ajax_cors(){
+    var xhr=createXHR();
+    xhr.onreadystatechange=function(){
+        console.log(this);
+        if(xhr.readyState == 4){
+            if((xhr.status >= 200 && xhr.status <300)|| xhr.status ==304){
+                console.log(xhr.responseText);
+            }else{
+                console.log(xhr.status);
+            }
+        }
+
+    };
+    //超时设置
+    xhr.timeout=1000;
+    xhr.ontimeout=function(){};
+    xhr.open('get','example.txt',true);//本地资源：相对url，远程资源：绝对url
+    //不可以setRequestHeader设置头部信息
+
+    xhr.send(null);
+
+}
+
+
+/**
+ * 跨浏览器的CORS
+ */
+function createCORSRequest(method,url){
+    var xhr=new XMLHttpRequest();
+    if('withCredentials' in xhr){//Chrome/FF/Safari
+        xhr.open(method,url,true);
+    }else if(typeof XDomainRequest != 'undefined'){
+        xhr=new XDomainRequest();
+        xhr.open(method,url);//xdr只能异步
+    }else{
+        xhr=null;
+    }
+
+    return xhr;
+}
+
+
